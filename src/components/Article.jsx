0eb82d906a43cc09fe75faaec65bc058c8899d41
alemalagni <alemalagni@gmail.com>
2025-04-09 14:51:07 +0200

    style(Article): added

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,6 +1,7 @@
 export default function Article({ text }) {
     return (
-        <div style={"border: 2px solid #000000"}>
+        <div style={{ border: '2px solid #000000', width: '20%', margin: '10px 0px', placeItems: 'center' }}
+        >
             <h2>{text}</h2>
         </div>
     );
