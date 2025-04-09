e83e79e0133e6f76f3ad2d76e64dcb73beb47289
alemalagni <alemalagni@gmail.com>
2025-04-09 15:11:21 +0200

    feat(App): add new function

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
+import { useState } from "react";
 import Articoli from "./Articoli"
 import Article from "./components/Article"
 
-export default function App() {
+function App() {
+
+  const [newArticle, setNewArticle] = useState('');
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    nuovoArticolo(newArticle)
+  }
+
   return (
     <>
+      <form onSubmit={handleSubmit}>
+        <input type="text"
+          value={newArticle}
+          onChange={e => { setNewArticle(e.target.value) }}
+        />
+
+      </form>
       {Articoli.map((item) => (
         <Article
+          key={item}
           text={item}
         />
       ))}
     </>
   );
 }
+
+function nuovoArticolo(newArticle) {
+  console.log(newArticle);
+  Articoli.push(newArticle);
+}
+
+export default App;
